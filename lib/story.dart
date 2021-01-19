b559e8b9dac269f139111ae53c483a190ae30786
irfanagust <agustiawanirfan866@gmail.com>
2021-01-19 11:54:25 +0700

    membuat fungsi reset story number dan nextquestion

--- lib/story.dart
+++ lib/story.dart
@@ -5,9 +5,9 @@ class Story{
   String storyTitle, choice1, choice2;
 
 //TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
-  Story(String title, String userChoice1, String userChoice2){
-    storyTitle = title;
-    choice1 = userChoice1;
-    choice2 = userChoice2;
+  Story({String storyTitle, String choice1, String choice2}){
+    this.storyTitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
   }
 }
